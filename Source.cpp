5389d34714ca166a86876b541c960ddd72e00991
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-01-07 00:38:50 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
+#include <cmath>
 
 const double epsilon = 0.000001;
 
-void calcEquationCoeff(double [],double[], double[]);
+void calcEquationCoeff(double[], double[], double[]);
 void printEquation(double[]);
+void printParabola(double[]);
 void simplify(double[]);
 bool pointLies(double[], double[]);
-void parallelLine(double[], double[],double[]);
+void parallelLine(double[], double[], double[]);
 void perpendicularLine(double[], double[], double[]);
 double getAbs(const double);
 void intersectionPoint(double[], double[], double[]);
 void getMidPoint(double[], double[], double[]);
-
+void printAltitudes(double[][2], double[][3]);
+void printMedians(double[][2], double[][2]);
+void printSymetrals(double[][3], double[][2]);
+double calculateArea(double[][2]);
+double calculatePerimeter(double[][2]);
+double calculateDistance(double[], double[]);
+bool quadraticEquation(double[],double[]);
+double valueOfLinFunc(double[], double);
 void welcomeMessage();
+void triangleMessage(char&);
 
 void operationLineInput();
 void operationPointLies();
@@ -20,19 +30,17 @@ void operationParallel();
 void operationPerpendicular();
 void operationIntersectLines();
 void operationTriangle();
+void operationIntersectParabolaLine();
 
 void readLineCoeffs(double[]);
 void readLineCoordinates(double[], const int);
+void readParabolaCoeffs(double[]);
 bool validateCoordinates(double[][2], int); // checks if all points have unique coordinates
-double calcArea(double[][2]);
 
-//proverka dali koordinatite sa razlichni
+// zakruglqvane
 //sukrushtavane na koef
 //imena na tochki i pravi!!!!!!!
-//void ili pointer masiv???
-//a=0 i b=0 ednovremenno
-// funkciqta za perpendikulqra i petata
-//razl naredeni dvoyki
+
 
 int main()
 {
@@ -64,6 +72,8 @@ int main()
 		case '4': operationPerpendicular(); break;
 		case '5': operationIntersectLines(); break;
 		case '6': operationTriangle(); break;
+		case '7': break;
+		case '8': operationIntersectParabolaLine();  break;
 		}
 		std::cout << "----------------------------\n";
 		std::cout << "Enter the number of operation you want to execute >> ";
@@ -78,13 +88,38 @@ double getAbs(double num)
 	return (num >= 0) ? num : -num;
 }
 
-void calcEquationCoeff(double firstPoint[],double secondPoint[], double lineCoeff[])
+bool quadraticEquation(double coeffs[], double roots[])
 {
 
-	lineCoeff[0] =secondPoint[1] - firstPoint[1];
+	double discriminamt = coeffs[1] * coeffs[1] - 4 * coeffs[0] * coeffs[2];
+
+	if (discriminamt < 0)
+	{
+		std::cout << "No real roots!\n";
+		return false;
+	}
+	else if(discriminamt>=0)
+	{
+		roots[0] = (-coeffs[1] + sqrt(discriminamt)) / (2 * coeffs[0]);
+		roots[1] = (-coeffs[1] - sqrt(discriminamt)) / (2 * coeffs[0]);
+	}
+	return true;
+}
+
+double valueOfLinFunc(double lineCoeffs[], double var)
+{
+	double value = -(lineCoeffs[0] * var + lineCoeffs[2]) / lineCoeffs[1];
+
+	return value;
+}
+
+void calcEquationCoeff(double firstPoint[], double secondPoint[], double lineCoeff[])
+{
+
+	lineCoeff[0] = secondPoint[1] - firstPoint[1];
 	lineCoeff[1] = firstPoint[0] - secondPoint[0];
 	lineCoeff[2] = secondPoint[0] * firstPoint[1] - firstPoint[0] * secondPoint[1];
-	
+
 }
 
 void printEquation(double lineCoeff[])
@@ -92,7 +127,7 @@ void printEquation(double lineCoeff[])
 
 
 	simplify(lineCoeff);
-	if (lineCoeff[0] != 0) 
+	if (lineCoeff[0] != 0)
 	{
 		if (lineCoeff[0] == 1)
 		{
@@ -100,11 +135,11 @@ void printEquation(double lineCoeff[])
 		}
 		else if (lineCoeff[0] == -1)
 		{
-			std::cout << '-'<<'x';
+			std::cout << '-' << 'x';
 		}
 		else
 		{
-			std::cout << lineCoeff[0]<<'x';
+			std::cout << lineCoeff[0] << 'x';
 		}
 	}
 
@@ -123,7 +158,7 @@ void printEquation(double lineCoeff[])
 			}
 		}
 		else if (lineCoeff[1] == -1)
-		{	
+		{
 			std::cout << '-' << 'y';
 		}
 		else
@@ -136,11 +171,11 @@ void printEquation(double lineCoeff[])
 			{
 				if (lineCoeff[1] > 0)
 				{
-					std::cout <<'+'<<lineCoeff[1] << 'y';
+					std::cout << '+' << lineCoeff[1] << 'y';
 				}
 				else
 				{
-					std::cout << lineCoeff[1]<<'y';
+					std::cout << lineCoeff[1] << 'y';
 				}
 			}
 		}
@@ -157,19 +192,68 @@ void printEquation(double lineCoeff[])
 			std::cout << '+' << lineCoeff[2];
 		}
 	}
-	std::cout << '='<<0<<std::endl;
+	std::cout << '=' << 0 << std::endl;
+}
+
+void printParabola(double parabolaCoeffs[])
+{
+	std::cout <<"y=";
+
+	if (parabolaCoeffs[0] == 1)
+	{
+		std::cout << "x^2";
+	}
+	else if (parabolaCoeffs[0] == -1)
+	{
+		std::cout << "-x^2";
+	}
+	else
+	{
+		std::cout << parabolaCoeffs[0] << "x^2";
+	}
+
+	if (parabolaCoeffs[1] != 0)
+	{
+		if (parabolaCoeffs[1] == 1)
+		{
+			std::cout << "+x";
+		}
+		else if (parabolaCoeffs[1] == -1)
+		{
+			std::cout << "-x";
+		}
+		else
+		{
+			if (parabolaCoeffs[1] > 0)
+			{
+				std::cout << '+';
+			}
+			std::cout << parabolaCoeffs[1] << 'x';
+		}
+	}
+	if (parabolaCoeffs[2] != 0)
+	{
+		if (parabolaCoeffs[2] > 0)
+		{
+			std::cout << '+' << parabolaCoeffs[2];
+		}
+		else
+		{
+			std::cout << parabolaCoeffs[2];
+		}
+	}
 }
 
 void simplify(double lineCoeff[])
 {
-	if (lineCoeff[0] < 0 || (lineCoeff[0]==0 && lineCoeff[1]<0))
+	if (lineCoeff[0] < 0 || (lineCoeff[0] == 0 && lineCoeff[1] < 0))
 	{
 		for (int i = 0; i < 3; i++)
 		{
 			lineCoeff[i] *= -1;
 		}
 	}
-} 
+}
 
 bool pointLies(double coordinates[], double lineCoeff[])
 {
@@ -187,14 +271,14 @@ void parallelLine(double pointCoordinates[], double lineCoeff[], double parallel
 {
 	parallelCoeff[0] = lineCoeff[0];
 	parallelCoeff[1] = lineCoeff[1];
-	parallelCoeff[2] = -(parallelCoeff[0]*pointCoordinates[0]+parallelCoeff[1]*pointCoordinates[1]);
+	parallelCoeff[2] = -(parallelCoeff[0] * pointCoordinates[0] + parallelCoeff[1] * pointCoordinates[1]);
 }
 
 void perpendicularLine(double pointCoordinates[], double lineCoeff[], double perpendicularCoeff[])
 {
-		perpendicularCoeff[0] = -lineCoeff[1];
-		perpendicularCoeff[1] = lineCoeff[0];
-		perpendicularCoeff[2] = -(perpendicularCoeff[0] * pointCoordinates[0] + perpendicularCoeff[1] * pointCoordinates[1]);
+	perpendicularCoeff[0] = -lineCoeff[1];
+	perpendicularCoeff[1] = lineCoeff[0];
+	perpendicularCoeff[2] = -(perpendicularCoeff[0] * pointCoordinates[0] + perpendicularCoeff[1] * pointCoordinates[1]);
 }
 
 void intersectionPoint(double firstLine[], double secondLine[], double intersection[])
@@ -210,18 +294,18 @@ void intersectionPoint(double firstLine[], double secondLine[], double intersect
 	{
 		if (getAbs(detA) <= epsilon && getAbs(detB) <= epsilon)
 		{
-			std::cout << "Lines coincide"<<'\n';
+			std::cout << "Lines coincide" << '\n';
 		}
 		else
 		{
-			std::cout << "Lines are parallel. They don't intersect."<<'\n';
+			std::cout << "Lines are parallel. They don't intersect." << '\n';
 		}
 	}
 	else
 	{
 		intersection[0] = detA / det;
 		intersection[1] = detB / det;
-		std::cout << intersection[0] << ' ' << intersection[1]<<'\n';
+		std::cout << intersection[0] << ' ' << intersection[1] << '\n';
 	}
 }
 
@@ -241,7 +325,7 @@ void welcomeMessage()
 	std::cout << "3. Determine the equation of the line that passes through a point and is parallel to another line\n";
 	std::cout << "4. Determine the equation of the line that is perpendicular to another line and passes through a point that lies on the given line \n";
 	std::cout << "5. Find the intersection point of two lines, if it exists\n";
-	std::cout << "6. Determine the equations of all altitudes, medians and symetrals of a triangle defined by the coordinates of its edges\n";
+	std::cout << "6. Determine the equations of all altitudes, medians and symetrals of a triangle defined by the coordinates of its vertices\n";
 	std::cout << "7. Find the tangent of a parabolyc equation through a point\n";
 	std::cout << "8. Determine the intersection point of a line and a parabola\n";
 	std::cout << "9. Determime if four lines form a quadrilateral and its type. If they don't, show an appropriate message\n\n";
@@ -259,20 +343,20 @@ void operationLineInput()
 	std::cin >> option;
 
 	while (true)
+	{
+		if (option == 'a' || option == 'b')
 		{
-			if (option == 'a' || option == 'b')
-			{
-				break;
-			}
-			else
-			{
-				std::cout << "Invalid input! Please, enter a OR b >> \n";
-				std::cin >> option;
-			}	
+			break;
+		}
+		else
+		{
+			std::cout << "Invalid input! Please, enter a OR b >> \n";
+			std::cin >> option;
+		}
 	}
 	if (option == 'a')
 	{
-		readLineCoordinates(lineCoeff,2);
+		readLineCoordinates(lineCoeff, 2);
 	}
 	else
 	{
@@ -292,7 +376,7 @@ void operationPointLies()
 	std::cin >> pointCoordinates[0];
 	std::cout << "y = ";
 	std::cin >> pointCoordinates[1];
-	std::cout << '(' << pointCoordinates[0] << ';' << pointCoordinates[1] << ')'<<'\n';
+	std::cout << '(' << pointCoordinates[0] << ';' << pointCoordinates[1] << ')' << '\n';
 	readLineCoeffs(lineCoeff);
 	printEquation(lineCoeff);
 	if (pointLies(pointCoordinates, lineCoeff))
@@ -328,7 +412,7 @@ void operationPerpendicular()
 {
 	double lineCoeff[3]{};
 	double pointCoordinates[2]{};
-	
+
 	std::cout << "Enter point (x;y) coordinates: \n";
 	std::cout << "x = ";
 	std::cin >> pointCoordinates[0];
@@ -344,12 +428,12 @@ void operationPerpendicular()
 		std::cout << "Your new perpendicular line is: ";
 		printEquation(perpendicularCoef);
 	}
-	
+
 	else
 	{
-		std::cout << "The line doesn't pass through the point"<<'\n';
+		std::cout << "The line doesn't pass through the point" << '\n';
 	}
-	
+
 }
 
 void operationIntersectLines()
@@ -361,7 +445,7 @@ void operationIntersectLines()
 	readLineCoeffs(secondLineCoeffs);
 	printEquation(firstLineCoeffs);
 	printEquation(secondLineCoeffs);
-	intersectionPoint(firstLineCoeffs, secondLineCoeffs,intersect);
+	intersectionPoint(firstLineCoeffs, secondLineCoeffs, intersect);
 }
 
 void operationTriangle()
@@ -369,12 +453,11 @@ void operationTriangle()
 	double vertices[3][2]{};
 	double midPoints[3][2]{};
 	double sideEquationCoeff[3][3]{};
-	double altitudeEquationCoeffs[3][3]{};
-	double medianEquationCoeffs[3][3]{};
-	double symetralEquationCoeffs[3][3]{};
+
 	double area = 0.00;
 	int counter = 0;
-
+	char optionTriangle = 'a';
+	
 	while (true)
 	{
 		for (int i = 0; i < 3; i++)
@@ -383,17 +466,18 @@ void operationTriangle()
 			std::cin >> vertices[i][0] >> vertices[i][1];
 		}
 
-		area = calcArea(vertices);
+		area = calculateArea(vertices);
 
 		if (area <= epsilon)
 		{
-			std::cout << "Triangle with such coordinates doesn't exist. Please, enter your coordinates again"<<'\n';
+			std::cout << "Triangle with such coordinates doesn't exist. Please, enter your coordinates again" << '\n';
 		}
 		else
 		{
 			break;
 		}
 	}
+	std::cout << "Your triangle has equations : \n";
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = i + 1; j < 3; j++)
@@ -404,19 +488,107 @@ void operationTriangle()
 			counter++;
 		}
 	}
+
+	triangleMessage(optionTriangle);
+
+	switch (optionTriangle)
+	{
+	case 'a': printAltitudes(vertices, sideEquationCoeff); break;
+	case 'b': printMedians(vertices, midPoints); break;
+	case 'c': printSymetrals(sideEquationCoeff, midPoints); break;
+	case 'd': std::cout << "Area = " << area<<'\n';
+	case 'e': std::cout << "Perimeter = " << calculatePerimeter(vertices) << '\n'; break;
+	}
+}
+
+void operationIntersectParabolaLine()
+{
+	double parabolaCoeffs[3];
+	double lineCoeffs[3];
+	double newCoeffs[3]{};
+	double roots[2]{};
+
+	readParabolaCoeffs(parabolaCoeffs);
+	readLineCoeffs(lineCoeffs);
+
+	std::cout << "Your parabola is: ";
+	printParabola(parabolaCoeffs);
 	std::cout << '\n';
-	counter = 2;
+	std::cout << "Your line is: ";
+	printEquation(lineCoeffs);
+	std::cout << '\n';
+
+	newCoeffs[0] = parabolaCoeffs[0] * lineCoeffs[1];
+	newCoeffs[1] = parabolaCoeffs[1] * lineCoeffs[1] + lineCoeffs[0];
+	newCoeffs[2] = parabolaCoeffs[2] * lineCoeffs[1] + lineCoeffs[2];
+
+	if (quadraticEquation(newCoeffs, roots))
+	{
+		if (roots[0] == roots[1]) // when D=0 we have a double root
+		{
+			double value = valueOfLinFunc(lineCoeffs, roots[0]);
+			std::cout << '(' << roots[0] << " ; " << value << ')';
+			std::cout << '\n';
+		}
+		else
+		{
+			double value1 = valueOfLinFunc(lineCoeffs, roots[0]);
+			double value2 = valueOfLinFunc(lineCoeffs, roots[1]);
+			std::cout << "Two intersection points: ";
+			std::cout << '('<<roots[0] <<" ; "<< value1 <<')';
+			std::cout << " (" << roots[1] << " ; " << value2 << ')';
+			std::cout << '\n';
+		}
+	}
+}
+
+void triangleMessage(char& optionTriangle)
+{
+	std::cout << "Choose an operation you want to execute with your triangle:\n\n";
+	std::cout << "	a. find equations of the altitudes\n";
+	std::cout << "	b. find equations of the medians\n";
+	std::cout << "	c. find equations of the symetrals\n";
+	std::cout << "	d. calculate area\n";
+	std::cout << "	e. calculate perimeter\n\n";
+	std::cout << "Enter your choice here>> ";
+	std::cin >> optionTriangle;
+
+	while (true)
+	{
+		if (optionTriangle >= 'a' && optionTriangle <= 'f')
+		{
+			break;
+		}
+		else if ((optionTriangle >= 'A' && optionTriangle <= 'F'))
+		{
+			optionTriangle = 'a' + (optionTriangle - 'A');
+			break;
+		}
+
+		std::cout << "You have entered an invalid input. Please, try again >> ";
+		std::cin >> optionTriangle;
+	}
+	std::cout << '\n';
+}
+
+void printAltitudes(double vertices[][2], double sideEquationCoeff[][3])
+{
+	int counter = 2;
+	double altitudeEquationCoeffs[3][3]{};
 	std::cout << "Altitudes: \n";
 
 	for (int i = 0; i < 3; i++)
-	{	
-		perpendicularLine(vertices[counter], sideEquationCoeff[i],altitudeEquationCoeffs[i]);
+	{
+		perpendicularLine(vertices[counter], sideEquationCoeff[i], altitudeEquationCoeffs[i]);
 		printEquation(altitudeEquationCoeffs[i]);
 		counter--;
 	}
+}
 
-	counter = 2;
-	std::cout << '\n';
+void printMedians(double vertices[][2], double midPoints[][2])
+{
+	int counter = 2;
+	double medianEquationCoeffs[3][3]{};
 	std::cout << "Medians: \n";
 	for (int i = 0; i < 3; i++)
 	{
@@ -424,14 +596,40 @@ void operationTriangle()
 		printEquation(medianEquationCoeffs[i]);
 		counter--;
 	}
+}
 
-	std::cout << '\n';
+void printSymetrals(double sideEquationCoeff[][3], double midPoints[][2])
+{
+	
+	double symetralEquationCoeffs[3][3]{};
 	std::cout << "Symetrals: \n";
 	for (int i = 0; i < 3; i++)
 	{
 		perpendicularLine(midPoints[i], sideEquationCoeff[i], symetralEquationCoeffs[i]);
 		printEquation(symetralEquationCoeffs[i]);
 	}
+}
+
+double calculatePerimeter(double vertices[][2])
+{
+	double perimeter = 0.00;
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = i + 1; j < 3; j++)
+		{
+			perimeter += calculateDistance(vertices[i], vertices[j]);
+		}
+	}
+	return perimeter;
+}
+
+double calculateDistance(double point1[], double point2[])
+{
+	double distance = (point2[0]-point1[0]) * (point2[0] - point1[0]) + (point2[1] - point1[1]) * (point2[1] - point1[1]);
+	distance = sqrt(distance);
+
+	return distance;
 	
 }
 
@@ -443,7 +641,25 @@ void readLineCoeffs(double lineCoeff[])
 		std::cin >> lineCoeff[0] >> lineCoeff[1] >> lineCoeff[2];
 		if (getAbs(lineCoeff[0]) <= epsilon && getAbs(lineCoeff[1]) <= epsilon)
 		{
-			std::cout<<"Coefficents a and be CAN'T be both  zeros simultaneously. Try again>> ";
+			std::cout << "Coefficents a and be CAN'T be both  zeros simultaneously. Try again>> ";
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void readParabolaCoeffs(double parabolaCoeffs[])
+{
+	std::cout << "Your parabola has equation y=ax^2+bx+c, where a=/=0. Please enter the coefficents a, b, c respectfully>> ";
+
+	while (true) //validation
+	{
+		std::cin >> parabolaCoeffs[0] >> parabolaCoeffs[1] >> parabolaCoeffs[2];
+		if (getAbs(parabolaCoeffs[0]) <= epsilon)
+		{
+			std::cout << "Coefficent a can't be zero. Try again>> ";
 		}
 		else
 		{
@@ -453,7 +669,7 @@ void readLineCoeffs(double lineCoeff[])
 }
 
 void readLineCoordinates(double lineCoeff[], const int count)
-{ 
+{
 	double coordinates[2][2]{};
 	while (true)
 	{
@@ -475,7 +691,7 @@ void readLineCoordinates(double lineCoeff[], const int count)
 			std::cout << "You have entered the same coordinates. Try again\n";
 		}
 	}
-			calcEquationCoeff(coordinates[0], coordinates[1], lineCoeff);
+	calcEquationCoeff(coordinates[0], coordinates[1], lineCoeff);
 }
 
 bool validateCoordinates(double coordinates[][2], const int count)
@@ -493,9 +709,9 @@ bool validateCoordinates(double coordinates[][2], const int count)
 	return true;
 }
 
-double calcArea(double coord[][2])
+double calculateArea(double coord[][2])
 {
-	double result = 0.5 * (coord[0][0]*(coord[1][1]-coord[2][1])+coord[0][1]*(coord[2][1]-coord[0][1])+coord[2][0]*(coord[0][1]-coord[1][1]));
+	double result = 0.5 * (coord[0][0] * (coord[1][1] - coord[2][1]) + coord[1][0] * (coord[2][1] - coord[0][1]) + coord[2][0] * (coord[0][1] - coord[1][1]));
 	result = getAbs(result);
 	return result;
 }
